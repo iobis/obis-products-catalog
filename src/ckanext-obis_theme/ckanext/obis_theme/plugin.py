86bc83a4cfbd0ce53c1ecf1da4f3239cdef83e16
Stephen Formel <steve@thedatapatch.com>
2026-02-18 22:15:40 +0000

    Replace Dataset with Product throughout UI via i18n override
    - Add English translation override in obis_theme (i18n/en/LC_MESSAGES/ckan.po)
    - Add ITranslation interface to ObisThemePlugin
    - All CKAN UI labels now show Product/Products instead of Dataset/Datasets

--- src/ckanext-obis_theme/ckanext/obis_theme/plugin.py
+++ src/ckanext-obis_theme/ckanext/obis_theme/plugin.py
@@ -1,11 +1,15 @@
+import os
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
 from ckanext.obis_theme import helpers
 
 
-class ObisThemePlugin(plugins.SingletonPlugin):
+class ObisThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.ITranslation)
 
     # IConfigurer
 
@@ -13,6 +17,17 @@ class ObisThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "obis_theme")
+
+    # ITranslation
+
+    def i18n_directory(self):
+        return os.path.join(os.path.dirname(__file__), "i18n")
+
+    def i18n_locales(self):
+        return ["en"]
+
+    def i18n_domain(self):
+        return "ckan"
 
     # ITemplateHelpers
 
