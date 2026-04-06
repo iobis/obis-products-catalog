78f513f576dbca33e517419641201ae32d8c2a98
Stephen Formel <steve@thedatapatch.com>
2026-04-06 22:59:15 +0000

    Closes #25: make facet sidebar collapsible
    Facets on the dataset browse page are now collapsible, starting closed
    by default. Clicking a facet heading expands/collapses it with an
    animated caret indicator.
    
    - Override facet_list.html snippet with Bootstrap 3 collapse markup
    - Register and load obis_theme JS asset via webassets.yml and base.html
    - Handle caret rotation via Bootstrap collapse events in script.js

--- src/ckanext-obis_theme/ckanext/obis_theme/assets/script.js
+++ src/ckanext-obis_theme/ckanext/obis_theme/assets/script.js
@@ -8,3 +8,23 @@ ckan.module("obis_theme-module", function ($, _) {
     initialize: function () {},
   };
 });
+
+$(document).ready(function() {
+    // Facet collapse toggle
+    $('[data-toggle="collapse"]').off('click').on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $(target).collapse('toggle');
+    });
+
+    // Rotate caret based on collapse state
+    $(document).on('show.bs.collapse', '.collapse', function() {
+        var id = $(this).attr('id');
+        $('[href="#' + id + '"] .facet-caret').css('transform', 'rotate(180deg)');
+    });
+
+    $(document).on('hide.bs.collapse', '.collapse', function() {
+        var id = $(this).attr('id');
+        $('[href="#' + id + '"] .facet-caret').css('transform', 'rotate(0deg)');
+    });
+});
